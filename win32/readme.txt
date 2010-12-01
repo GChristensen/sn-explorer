8dd04f34aefd92d52f1c9e3f62c9c1794b2b8eee
GChristensen <gchristnsn@gmail.com>
2010-12-01 09:55:12 +0300

    reneval

--- win32/readme.txt
+++
@@ -1,5 +0,0 @@
-Win32 utilities for sn-explorer-gui application.
-
-(C) 2010, g/christensen (gchristnsn@gmail.com)
-
-Use mingw32 to build.
--- win32/sn-explorer.h
+++
@@ -1,8 +0,0 @@
-
-#ifndef __SN_EXPLORER__
-
-#define __SN_EXPLORER__
-
-#define TRAYMENU_WND_CLASS _T("sn-explorer-tray-menu-wnd")
-
-#endif
